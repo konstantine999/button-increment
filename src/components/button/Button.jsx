8e7086f1157a9acf1ad5fd636d952ff87c56ecce
konstantine999 <kotemuzashvili0@gmail.com>
2023-05-16 15:41:38 +0400

    function change

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,19 +3,22 @@ import "./button.css";
 
 function Button() {
   const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
+  const incrementDecrement = (a) => {
+    setCount(count + a);
   };
   return (
     <div className="buttons-wrapper">
-      <button className="increment-button" onClick={decrement}>
+      <button
+        className="increment-button"
+        onClick={() => incrementDecrement(-1)}
+      >
         -
       </button>
       <button className="main-button">I was Clicked {count} times</button>
-      <button className="increment-button" onClick={increment}>
+      <button
+        className="increment-button"
+        onClick={() => incrementDecrement(1)}
+      >
         +
       </button>
     </div>
